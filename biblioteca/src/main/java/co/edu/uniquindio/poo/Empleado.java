215415f8697eaa56980c97032c612e6a3a713be2
nicolmarin <nicolmoreno090905@gmail.com>
2024-08-24 13:41:44 -0500

    empleado con sus atributos

---
+++ biblioteca/src/main/java/co/edu/uniquindio/poo/Empleado.java
@@ -0,0 +1,42 @@
+package co.edu.uniquindio.poo;
+
+
+public abstract class Empleado  {
+    private String nombre;
+    private int idEmpleado;
+    
+
+
+    public Empleado(String nombre,int idEmpleado){
+        this.nombre=nombre;
+        this.idEmpleado=idEmpleado;
+        
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+
+    
+
+    
+
+    
+}
