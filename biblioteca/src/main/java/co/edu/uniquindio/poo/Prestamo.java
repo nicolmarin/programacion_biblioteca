17470e92e56e73cbc37800f7f49d31f47e342484
nicolmarin <nicolmoreno090905@gmail.com>
2024-08-24 18:56:05 -0500

    cambios prestamo

--- biblioteca/src/main/java/co/edu/uniquindio/poo/Prestamo.java
+++ biblioteca/src/main/java/co/edu/uniquindio/poo/Prestamo.java
@@ -1,54 +1,54 @@
 package co.edu.uniquindio.poo;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public class Prestamo {
-    private Libro libro;
-    private Miembro miembro;
-    private Date fechaPrestamo;
-    private Date fechaDevolucion;
+    private libro Libro;
+    private miembro Miembro;
+    private LocalDateTime fechaPrestamo;
+    private LocalDateTime fechaDevolucion;
 
-
-    public Prestamo(Libro libro, Miembro miembro, Date fechaPrestamo, Date fechaDevolucion) {
-        this.libro = libro;
-        this.miembro = miembro;
+    public Prestamo(libro libro, miembro miembro, Date fechaPrestamo, Date fechaDevolucion) {
+        Libro = libro;
+        Miembro = miembro;
         this.fechaPrestamo = fechaPrestamo;
-        this.fechaDevolucion = fechaDevolucion;
-    }
-
-
-    public Libro getLibro() {
-        return libro;
-    }
-
-    public void setLibro(Libro libro) {
-        this.libro = libro;
-    }
-
-    public Miembro getMiembro() {
-        return miembro;
-    }
-
-    public void setMiembro(Miembro miembro) {
-        this.miembro = miembro;
-    }
-
-    public Date getFechaPrestamo() {
-        return fechaPrestamo;
-    }
-
-    public void setFechaPrestamo(Date fechaPrestamo) {
-        this.fechaPrestamo = fechaPrestamo;
-    }
-
-    public Date getFechaDevolucion() {
-        return fechaDevolucion;
-    }
-
-    public void setFechaDevolucion(Date fechaDevolucion) {
         this.fechaDevolucion = fechaDevolucion;
     }
 }
+
+    public libro getLibro() {
+        return Libro;
+    }
+
+    public void setLibro(libro libro) {
+        Libro = libro;
+    }
+
+    public miembro getMiembro() {
+        return Miembro;
+    }
+
+    public void setMiembro(miembro miembro) {
+        Miembro = miembro;
+    }
+
+    public LocalDateTime getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(LocalDateTime fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public LocalDateTime getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(LocalDateTime fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+
 
 
 
