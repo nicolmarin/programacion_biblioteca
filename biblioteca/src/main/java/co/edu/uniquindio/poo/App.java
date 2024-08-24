2641d2126cb20978a8670a9236e69852a5396211
AnduellgDeveloper <anduellgames@gmail.com>
2024-08-24 18:05:57 -0500

    Clase Prestamo

--- biblioteca/src/main/java/co/edu/uniquindio/poo/App.java
+++ biblioteca/src/main/java/co/edu/uniquindio/poo/App.java
@@ -1,9 +1,5 @@
 package co.edu.uniquindio.poo;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
---
+++ biblioteca/src/main/java/co/edu/uniquindio/poo/Prestamo.java
@@ -0,0 +1,1 @@
+package co.edu.uniquindio.poo;
