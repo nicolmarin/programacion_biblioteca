631df9d8fe1689008ef800f864951b6a66164a28
nicolmarin <nicolmoreno090905@gmail.com>
2024-08-24 19:16:03 -0500

    correciones

--- biblioteca/src/main/java/co/edu/uniquindio/poo/Miembro.java
+++ biblioteca/src/main/java/co/edu/uniquindio/poo/Miembro.java
@@ -1,28 +1,38 @@
 package co.edu.uniquindio.poo;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Miembro {
     private String nombre;
     private int idMiembro;
-    private List<Prestamo> prestamosActivos; // El nombre estaba en singular
-
+    private List<Prestamo>prestamoActivos;
+    
+    
     public Miembro(String nombre, int idMiembro) {
         this.nombre = nombre;
         this.idMiembro = idMiembro;
-        this.prestamosActivos = new ArrayList<>();
+        this.prestamosActivos=new ArrayList<>();
+    }
+    public void agregarPrestamos(Prestamo prestamo){
+        prestamoActivos.add(prestamo);
+
+    }
+    public void removerPrestamo(Prestamo prestamo){
+        prestamoActivos.remove(prestamo);
     }
 
-    public void mostrarPrestamosActivos() {
-        if (prestamosActivos.isEmpty()) {
-            System.out.println("No hay préstamos activos para el miembro " + nombre);
-        } else {
-            System.out.println("Préstamos activos para el miembro " + nombre + ":");
-            for (Prestamo prestamo : prestamosActivos) {
-                System.out.println("Libro: " + prestamo.getLibro().getTitulo() + ", Fecha de Préstamo: " + prestamo.getFechaPrestamo());
-            }
-        }
+    public List<Prestamo> getPrestamosActivos(){
+        return prestamoActivos;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public int getIdMiembro() {
+        return idMiembro;
     }
 
 
@@ -30,33 +40,21 @@ public class Miembro {
         this.nombre = nombre;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getIdMiembro() {
-        return idMiembro;
-    }
 
     public void setIdMiembro(int idMiembro) {
         this.idMiembro = idMiembro;
     }
 
-    public List<Prestamo> getPrestamosActivos() {
-        return prestamosActivos;
+
+    
+
+
+    public void setPrestamoActivos(List<Prestamo> prestamoActivos) {
+        this.prestamoActivos = prestamoActivos;
     }
 
-    public void setPrestamosActivos(List<Prestamo> prestamosActivos) {
-        this.prestamosActivos = prestamosActivos;
-    }
+    
 
-    public void agregarPrestamo(Prestamo prestamo) {
-        prestamosActivos.add(prestamo);
-    }
-
-    public void eliminarPrestamo(Prestamo prestamo) {
-        prestamosActivos.remove(prestamo);
-    }
-
-
+    
+    
 }
