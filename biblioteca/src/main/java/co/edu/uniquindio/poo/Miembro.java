77e679d5297e7e00df04c9b06411d9f83db6803d
AnduellgDeveloper <anduellgames@gmail.com>
2024-08-24 19:10:24 -0500

    Metodos nuevos en clase Miembro

--- biblioteca/src/main/java/co/edu/uniquindio/poo/Miembro.java
+++ biblioteca/src/main/java/co/edu/uniquindio/poo/Miembro.java
@@ -1,27 +1,28 @@
 package co.edu.uniquindio.poo;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Miembro {
     private String nombre;
     private int idMiembro;
-    private List<PrestamoActivos>prestamoActivos;
-    
-    
+    private List<Prestamo> prestamosActivos; // El nombre estaba en singular
+
     public Miembro(String nombre, int idMiembro) {
         this.nombre = nombre;
         this.idMiembro = idMiembro;
-        this.prestamosActivos=new ArrayList<>();
+        this.prestamosActivos = new ArrayList<>();
     }
 
-
-    public String getNombre() {
-        return nombre;
-    }
-
-
-    public int getIdMiembro() {
-        return idMiembro;
+    public void mostrarPrestamosActivos() {
+        if (prestamosActivos.isEmpty()) {
+            System.out.println("No hay préstamos activos para el miembro " + nombre);
+        } else {
+            System.out.println("Préstamos activos para el miembro " + nombre + ":");
+            for (Prestamo prestamo : prestamosActivos) {
+                System.out.println("Libro: " + prestamo.getLibro().getTitulo() + ", Fecha de Préstamo: " + prestamo.getFechaPrestamo());
+            }
+        }
     }
 
 
@@ -29,13 +30,33 @@ public class Miembro {
         this.nombre = nombre;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getIdMiembro() {
+        return idMiembro;
+    }
 
     public void setIdMiembro(int idMiembro) {
         this.idMiembro = idMiembro;
     }
 
-    
+    public List<Prestamo> getPrestamosActivos() {
+        return prestamosActivos;
+    }
 
-    
-    
+    public void setPrestamosActivos(List<Prestamo> prestamosActivos) {
+        this.prestamosActivos = prestamosActivos;
+    }
+
+    public void agregarPrestamo(Prestamo prestamo) {
+        prestamosActivos.add(prestamo);
+    }
+
+    public void eliminarPrestamo(Prestamo prestamo) {
+        prestamosActivos.remove(prestamo);
+    }
+
+
 }
